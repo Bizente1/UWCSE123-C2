98c891ee0efa5b3f6c20c93baec608b63d0aba96
Bizente1 <deleonbisente9@gmail.com>
2025-11-05 22:25:33 -0700

    Ive started working on the extension and improved the basic

--- Mondrian.java
+++ Mondrian.java
@@ -4,7 +4,7 @@ import java.awt.*;
 public class Mondrian {
 
     private final static Color[] COLOR = {Color.RED, Color.YELLOW, Color.CYAN, Color.WHITE};
-    private final static Random rand = new Random();
+    private final static Random RAND = new Random();
 
     Mondrian(){}
 
@@ -15,21 +15,22 @@ public class Mondrian {
 
     private void paintBasicMondrian(Color[][] pixels,  int startX, int endX, int startY, int endY){
 
-            int splitX = rand.nextInt(endX-startX)+startX;
-            int splitY = rand.nextInt(endY-startY)+startY;
-            System.out.println(endX);
-        if(endX-startX >= pixels.length/4 && endY-startY >= pixels[0].length/4){
+        int splitX = RAND.nextInt(endX-startX)+startX;
+        int splitY = RAND.nextInt(endY-startY)+startY;
+        int[][] splits = {{startX, splitX, startY, splitY}, {splitX, endX, startY, splitY}, 
+                            {startX, splitX, splitY, endY}, {splitX, endX, splitY, endY}};
 
-            paintBasicMondrian(pixels,  startX, splitX, startY, splitY); // top-left
-            paintBasicMondrian(pixels, splitX, endX, startY, splitY);   // top-right
-            paintBasicMondrian(pixels, startX, splitX, splitY, endY);   // bottom-left
-            paintBasicMondrian(pixels, splitX, endX, splitY, endY);     // bottom-right
+        if(endX-startX >= pixels.length/4 && endY-startY >= pixels[0].length/4){
+            for(int[] lines: splits){
+                paintBasicMondrian(pixels, lines[0], lines[1], lines[2], lines[3]);
+            }
+
         }else if(endY - startY >= pixels[0].length/4){
-            paintBasicMondrian(pixels, startX, endX, startY, splitY);   // top-right
-            paintBasicMondrian(pixels, startX, endX, splitY, endY);   // bottom-left 
+            paintBasicMondrian(pixels, startX, endX, startY, splitY);   // top
+            paintBasicMondrian(pixels, startX, endX, splitY, endY);   // bottom 
         }else if(endX - startX >= pixels.length/4){
-            paintBasicMondrian(pixels,  startX, splitX, startY, endY); // top-left
-            paintBasicMondrian(pixels, splitX, endX, startY, endY);   // top-right
+            paintBasicMondrian(pixels,  startX, splitX, startY, endY); // left
+            paintBasicMondrian(pixels, splitX, endX, startY, endY);   // right
         }else{
             fill(pixels, startX, endX, startY, endY);
         }
@@ -41,7 +42,31 @@ public class Mondrian {
     }
 
     private void paintComplexMondrian(Color[][] pixels,  int startX, int endX, int startY, int endY){
-        
+        int splitX = RAND.nextInt(endX-startX)+startX;
+        int splitY = RAND.nextInt(endY-startY)+startY;
+
+        if(endX-startX >= pixels.length/4 && endY-startY >= pixels[0].length/4){
+            int splitX2 = RAND.nextInt(endX-splitX)+splitX;
+            int splitY2 = RAND.nextInt(splitY-startY)+startY;
+            int[][] splits = {{startX, splitX, startY, splitY}, {splitX, endX, startY, splitY}, 
+                                {startX, splitX, splitY, endY}, {splitX, endX, splitY, endY}};
+            int numOfSplits = RAND.nextInt(4) +1;
+
+            for(int i = 0; i < numOfSplits; i++){
+                //paintComplexMondrian();
+            }
+
+            
+            
+        }else if(endY - startY >= pixels[0].length/4){
+            paintBasicMondrian(pixels, startX, endX, startY, splitY);   // top
+            paintBasicMondrian(pixels, startX, endX, splitY, endY);   // bottom 
+        }else if(endX - startX >= pixels.length/4){
+            paintBasicMondrian(pixels,  startX, splitX, startY, endY); // left
+            paintBasicMondrian(pixels, splitX, endX, startY, endY);   // right
+        }else{
+            fill(pixels, startX, endX, startY, endY);
+        }
     }
 
     private void argumentChecker(Color[][] pixels){
@@ -52,7 +77,7 @@ public class Mondrian {
 
     private static void fill(Color[][] pixels, int x1, int x2, int y1, int y2){
 
-        Color colorPicked = COLOR[rand.nextInt(4)];
+        Color colorPicked = COLOR[RAND.nextInt(4)];
         for(int i = x1 + 2; i < x2 -2; i++){
             for(int j = y1 + 2; j < y2 -2; j++){
                 pixels[i][j] = colorPicked;
